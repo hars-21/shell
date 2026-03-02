df4fdcb1d76d81e05682de8e2b469ee159b8004e
Harshil <harshilgupta.2005@gmail.com>
2026-03-02 19:42:24 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -21,25 +21,27 @@ fn main() {
 }
 
 fn run_commands(input: &str) {
-    let parts: Vec<&str> = input.split_whitespace().collect();
-    let command = parts[0];
+    let args: Vec<&str> = input.split_whitespace().collect();
+    let command = args[0];
 
     match command {
+        "cd" => env::set_current_dir(&args[1])
+            .unwrap_or_else(|_| println!("cd: {}: No such file or directory", &args[1])),
         "pwd" => println!("{}", env::current_dir().unwrap().display()),
-        "echo" => println!("{}", parts[1..].join(" ")),
-        "type" => match parts[1] {
-            "exit" | "echo" | "type" | "pwd" => println!("{} is a shell builtin", parts[1]),
+        "echo" => println!("{}", args[1..].join(" ")),
+        "type" => match args[1] {
+            "exit" | "echo" | "type" | "pwd" | "cd" => println!("{} is a shell builtin", args[1]),
             _ => {
-                if let Some(path) = find_executable_in_path(&parts[1]) {
-                    println!("{} is {}", parts[1], path.display());
+                if let Some(path) = find_executable_in_path(&args[1]) {
+                    println!("{} is {}", args[1], path.display());
                 } else {
-                    println!("{}: not found", parts[1]);
+                    println!("{}: not found", args[1]);
                 }
             }
         },
         _ => {
             if let Some(_path) = find_executable_in_path(&command) {
-                let _status = Command::new(command).args(&parts[1..]).status();
+                let _status = Command::new(command).args(&args[1..]).status();
             } else {
                 println!("{}: command not found", command);
             }
